b01c69a9ca59efbe792fb31d43447aabecef4bfe
Huy <Huy@DESKTOP-LK1ICH7>
2022-01-13 13:47:18 +0700

    Hibarnate

---
+++ src/main/java/com/huy/hibernate/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.huy.hibernate.service;
+
+import java.util.List;
+
+import com.huy.hibernate.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
